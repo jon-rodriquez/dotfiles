dabdac5430e2476b4427ce6d01dbda0e6eea6dc3
Jonathan Rodriquez <jrodriquez.07.02@gmail.com>
2024-08-27 01:54:27 -0400

    fix to definition on lunarvim. what a headache. treesitter text objects not working unsure what the issue is, has the method of installing changed?

--- lunarvim/lvim/config.lua
+++ lunarvim/lvim/config.lua
@@ -100,48 +100,50 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
+-- require'nvim-treesitter.configs'.setup {
+--   textobjects = {
+--     select = {
+--       enable = true,
 
--- lvim.builtin.treesitter.textobjects.select = {
---   enable = true,
+--       -- Automatically jump forward to textobj, similar to targets.vim
+--       lookahead = true,
 
---   -- Automatically jump forward to textobj, similar to targets.vim
---   lookahead = true,
-
---   keymaps = {
---     -- You can use the capture groups defined in textobjects.scm
---     ["af"] = "@function.outer",
---     ["if"] = "@function.inner",
---     ["ac"] = "@class.outer",
---     -- You can optionally set descriptions to the mappings (used in the desc parameter of
---     -- nvim_buf_set_keymap) which plugins like which-key display
---     ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
---     -- You can also use captures from other query groups like `locals.scm`
---     ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+--       keymaps = {
+--         -- You can use the capture groups defined in textobjects.scm
+--         ["af"] = "@function.outer",
+--         ["if"] = "@function.inner",
+--         ["ac"] = "@class.outer",
+--         -- You can optionally set descriptions to the mappings (used in the desc parameter of
+--         -- nvim_buf_set_keymap) which plugins like which-key display
+--         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+--         -- You can also use captures from other query groups like `locals.scm`
+--         ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+--       },
+--       -- You can choose the select mode (default is charwise 'v')
+--       --
+--       -- Can also be a function which gets passed a table with the keys
+--       -- * query_string: eg '@function.inner'
+--       -- * method: eg 'v' or 'o'
+--       -- and should return the mode ('v', 'V', or '<c-v>') or a table
+--       -- mapping query_strings to modes.
+--       selection_modes = {
+--         ['@parameter.outer'] = 'v', -- charwise
+--         ['@function.outer'] = 'V', -- linewise
+--         ['@class.outer'] = '<c-v>', -- blockwise
+--       },
+--       -- If you set this to `true` (default is `false`) then any textobject is
+--       -- extended to include preceding or succeeding whitespace. Succeeding
+--       -- whitespace has priority in order to act similarly to eg the built-in
+--       -- `ap`.
+--       --
+--       -- Can also be a function which gets passed a table with the keys
+--       -- * query_string: eg '@function.inner'
+--       -- * selection_mode: eg 'v'
+--       -- and should return true or false
+--       include_surrounding_whitespace = true,
+--     },
 --   },
---   -- You can choose the select mode (default is charwise 'v')
---   --
---   -- Can also be a function which gets passed a table with the keys
---   -- * query_string: eg '@function.inner'
---   -- * method: eg 'v' or 'o'
---   -- and should return the mode ('v', 'V', or '<c-v>') or a table
---   -- mapping query_strings to modes.
---   selection_modes = {
---     ['@parameter.outer'] = 'v', -- charwise
---     ['@function.outer'] = 'V',  -- linewise
---     ['@class.outer'] = '<c-v>', -- blockwise
---   },
---   -- If you set this to `true` (default is `false`) then any textobject is
---   -- extended to include preceding or succeeding whitespace. Succeeding
---   -- whitespace has priority in order to act similarly to eg the built-in
---   -- `ap`.
---   --
---   -- Can also be a function which gets passed a table with the keys
---   -- * query_string: eg '@function.inner'
---   -- * selection_mode: eg 'v'
---   -- and should return true of false
---   include_surrounding_whitespace = true,
 -- }
-
 -- generic LSP settings
 
 -- -- make sure server will always be installed even if the server is in skipped_servers list
@@ -213,35 +215,15 @@ lvim.plugins = {
       require('git-conflict').setup()
     end,
   },
-  { 'github/copilot.vim' },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    after = "nvim-treesitter",
-    dependacies = "nvim-treesitter/nvim-treesitter",
-  },
-  { 'simrat39/rust-tools.nvim' },
-  { 'MunifTanjim/nui.nvim' },
-  {
-    'VonHeikemen/fine-cmdline.nvim',
-    dependacies = {
-      { 'MunifTanjim/nui.nvim' }
-    }
-  }
-}
+  { 'github/copilot.vim' }
+  -- {'nvim-treesitter/nvim-treesitter'},
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-textobjects",
+  --   after = "nvim-treesitter",
+  --   dependacies = "nvim-treesitter/nvim-treesitter",
+  -- },
+ }
 
 vim.cmd [[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
 vim.g.copilot_no_tab_map = true
 
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
