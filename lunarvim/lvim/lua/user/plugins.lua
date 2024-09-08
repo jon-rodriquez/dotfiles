df7aaafcf8d78c0f53ec733304230b8eb36e7648
Jonathan Rodriquez <jrodriquez.07.02@gmail.com>
2024-09-08 11:36:32 -0400

    addition of markdown plug in

--- lunarvim/lvim/lua/user/plugins.lua
+++ lunarvim/lvim/lua/user/plugins.lua
@@ -1,4 +1,11 @@
 lvim.plugins = {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
   { 'christoomey/vim-tmux-navigator' },
   {
     'akinsho/git-conflict.nvim',
