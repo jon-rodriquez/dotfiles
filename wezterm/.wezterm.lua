101f491b3fcb13010602f81e03d0e851dedebf77
jon-rodriquez <jrodriquez0407@gmail.com>
2024-11-14 14:20:21 -0500

    init wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,15 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.adjust_window_size_when_changing_font_size = false
+config.debug_key_events = false
+config.enable_tab_bar = false
+config.native_macos_fullscreen_mode = false
+config.window_close_confirmation = "NeverPrompt"
+
+-- and finally, return the configuration to wezterm
+return config
