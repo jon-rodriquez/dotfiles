7c7aa8fba0fd0b9f9ea1ed1f8aa4e52246a39f44
jon-rodriquez <jrodriquez0407@gmail.com>
2024-11-15 08:44:16 -0500

    color scheme update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,6 +10,7 @@ config.debug_key_events = false
 config.enable_tab_bar = false
 config.native_macos_fullscreen_mode = false
 config.window_close_confirmation = "NeverPrompt"
+config.color_scheme = "Catppuccin Mocha"
 
 -- and finally, return the configuration to wezterm
 return config
